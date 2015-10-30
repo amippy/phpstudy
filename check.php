83086548f91025e0784ddc67b28f1fcf0ebc74d6
amippy <aminaka1223@gmail.com>
2015-10-30 23:47:48 +0900

    html4.01からhtml5に変更

--- check.php
+++ check.php
@@ -1,7 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="UTF-8">
 <title>PHP基礎</title>
 </head>
 <body>
